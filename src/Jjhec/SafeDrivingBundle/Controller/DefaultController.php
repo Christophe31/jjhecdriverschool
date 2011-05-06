4871cffd9bb889a3ab7f1a13c60a958260c843a2
Christophe31 <Christophe31@gmail.com>
2011-05-06 22:52:18 +0200

    use app template, render a value.

--- src/Jjhec/SafeDrivingBundle/Controller/DefaultController.php
+++ src/Jjhec/SafeDrivingBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('JjhecSafeDrivingBundle:Default:index.html.twig');
+        return $this->render('JjhecSafeDrivingBundle:Default:index.html.twig',array("text"=>"value"));
     }
 }
