604a2a1a0d37dbe75635140459c27cbd08837cba
Julien <Julien@localhost>
2011-06-07 18:12:44 +0200

    view_customers.html , ajout d'un menu d'action, avec javascript

--- crm/urls.py
+++ crm/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('crm.views',
     url('^$','selling', name='crm.index'),
     url('^list_customers$', 'view_customers', name='crm.view_customers'),
-    url('^billing/user/\d$', 'billing', name='crm.billing'),
-    url('^add_score/user/\d$', 'add_score', name='crm.add_score'),
-    url('^register_formation/user/\d$', 'register_formation', name='crm.register_formation'),
-    url('^register_exam/user/\d$', 'register_exam', name='crm.register_exam'),
+    url('^billing/user/(?P<user_id>\d)$', 'billing', name='billing'),
+    url('^add_score/user/(?P<user_id>\d)$', 'add_score', name='crm.add_score'),
+    url('^register_formation/user/(?P<user_id>\d+)$', 'register_formation', name='crm.register_formation'),
+    url('^register_exam/user/(?P<user_id>\d+)$', 'register_exam', name='crm.register_exam'),
 )
