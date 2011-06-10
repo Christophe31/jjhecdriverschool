1b4ab2d2fa0227f3221145a692fc2d4c7357e84b
Jerome SOMMET <som1islost@gmail.com>
2011-06-10 08:24:30 -0700

    update tpl : add footer

---
+++ media/jjhec/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("a.new_window").attr("target", "_blank");
+  	$(function(){
+		$("#faded").faded({
+			speed: 500,
+			crossfade: true,
+			autoplay: 10000,
+			autopagination:false
+		});
+		
+		$('#domain-form').jqTransform({imgPath:'jqtransformplugin/img/'});
+	});
+    Cufon.now();
+ });
