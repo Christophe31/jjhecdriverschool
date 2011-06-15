bdc4a664c0adf86e30de14b3c76d8d842595adf8
Christophe31 <Christophe31@gmail.com>
2011-06-15 14:29:50 +0200

    profile: add stats, jer?me need to work

--- profile/urls.py
+++ profile/urls.py
@@ -7,8 +7,9 @@ urlpatterns = patterns('profile.views',
     url('^login$', 'login', name='profile.login'),
     url('^logout$', 'logout', name='profile.logout'),
     url('bill/', 'bill', name='profile.bill'),
-    url('edit_profile/','edit_profile',name='profile.edit_profile'),
-    url('ajax_get_notes_range','ajax_get_notes_range',name='profile.ajax_get_notes_range')
+    url('edit_profile/', 'edit_profile', name='profile.edit_profile'),
+    url('ajax_get_notes_range', 'ajax_get_notes_range', name='profile.ajax_get_notes_range'),
+    url('get_code_marks', 'get_code_marks', name='profile.get_code_marks'),
 )
 
 # vim:set et sts=4 ts=4 tw=80:
