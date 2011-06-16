ed8791228dd1b3f6b37e29c012b9286c69f6cedb
Jerome SOMMET <jerome.sommet@supinfo.com>
2011-06-15 17:31:22 -0700

    Mod homepage with severals thinks
    add carousel for offer
    add info div with custom hight
    add datetimedatapiker(not tested)

--- media/jjhec/js/script.js
+++ media/jjhec/js/script.js
@@ -1,20 +1,31 @@
 $(function(){
-  $("a.new_window").attr("target", "_blank");
-  var e = $("#faded");
-  if (e.length != 0)
-    e.faded({
-	    speed: 500,
-	    crossfade: true,
-	    autoplay: 10000,
-	    autopagination:false
-	  });
+    $("a.new_window").attr("target", "_blank");
+    var e = $("#faded");
+    if (e.length != 0){
+        e.faded({
+            speed: 500,
+            crossfade: true,
+            autoplay: 10000,
+            autopagination:false
+        });
+     }   
 
-  if ((e = $('#domain-form')).length != 0)
-    e.jqTransform({imgPath:'jqtransformplugin/img/'});
-
-  if ((e = $('.dateinput')).length != 0)
-    e.datepicker();
-  Cufon.now();
+    if ((e = $('#domain-form')).length != 0){
+        e.jqTransform({imgPath:'jqtransformplugin/img/'});
+    }
+    if ((e = $('.dateinput')).length != 0){
+        e.datepicker();
+    }
+    if ((e = $('.datetimeinput')).length != 0){
+        e.datetimepicker();
+    }
+   if ((e = $('#mycarousel')).length != 0){    
+        $('#mycarousel').jcarousel({
+            scroll: '2',
+            animation: 'slow'
+        });  
+    }
+    Cufon.now();
  });
 
 function loginFormKeyPressed(field, evt){
