b8776f050822572ebcba60f9f471463f18c4e965
Christophe31 <Christophe31@gmail.com>
2011-06-16 14:48:28 +0200

    hello_world:view send formulas.

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from common import models
 
 # for each model, add, change and delete permission are created,
 # use a "@permission_required('type_model')" imply registred user.
@@ -9,7 +10,8 @@ from django.template import RequestContext
 def greetings(request, name=None):
     return render_to_response("hello_world/greetings.html",
                               {
-                                  "name": name or "no_name"
+                                  "formulas": models.Formula.objects.all(),
+                                  "name": name or "no_name",
                               },
                               context_instance=RequestContext(request)
                              )
