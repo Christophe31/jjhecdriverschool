6ed82124040750dbff9b00707007dbe4249b7d48
Christophe31 <Christophe31@gmail.com>
2011-06-19 19:22:45 +0200

    urls fix

--- trainer/urls.py
+++ trainer/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('trainer.views',
     url('^$','mark_client', name='trainer.index'),
-    url('view_vehicles', 'view_vehicles', name='trainer.view_vehicles'),
-    url('^Booom\!/on_vehicle/(?p<id>\d)$','declare_incident',name='trainer.declare_incident')
+    url('^view_vehicles$', 'view_vehicles', name='trainer.view_vehicles'),
+    url('^Booom\!/on_vehicle/(?P<id>\d)$','declare_incident',name='trainer.declare_incident')
     )
